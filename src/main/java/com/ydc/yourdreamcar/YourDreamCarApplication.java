a04511ccc962c9605eb3d42fb3b346467a5148f2
Belet <oguzhan.belet@solviads.com>
2022-06-02 17:15:06 +0300

    initialize project

---
+++ src/main/java/com/ydc/yourdreamcar/YourDreamCarApplication.java
@@ -0,0 +1,13 @@
+package com.ydc.yourdreamcar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YourDreamCarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YourDreamCarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ydc/yourdreamcar/YourDreamCarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ydc.yourdreamcar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YourDreamCarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
